476729f8f4712a2e233842d14d0809be463d752c
Anirudh Dhiman <anirudhdhiman@Anirudhs-MacBook-Air-2.local>
2026-04-24 17:17:31 +0530

    fix db init

--- database.py
+++ database.py
@@ -9,7 +9,10 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}  # needed for SQLite
+)
 
 SessionLocal = sessionmaker(
     bind=engine,
